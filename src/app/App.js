f125391f2b82663c0dca7721a449da281e35f67d
Artur Zhitkovich <artur.zhitkovich15041998@gmail.com>
2022-08-10 10:47:25 +0300

    Update App.js
    Вот так надо

--- src/app/App.js
+++ src/app/App.js
@@ -14,8 +14,7 @@ function App() {
                 <Route path="/" exact component={ Home } />
                 <Route path="/login" component={ Login } />
                 <Route path="/users/:userId?" component={ UsersList } />
-                <Route path="/404" component={ NotFoundPage } />
-                <Redirect to="/404" />
+                <Route path="*" component={ NotFoundPage } />
             </Switch>
         </>
     );
