139346b28ff136c3e69cb4e9c82076b6893d8bc0
Gregdabro <gregmonak@gmail.com>
2022-09-16 10:19:02 +0200

    implement qualitiesCard

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 import api from "../../../api";
 import Loader from "../../UI/Loader/Loader";
 import UserCard from "../../UI/userCard";
+import QualitiesCard from "../../UI/qualitiesCard";
 
 const UserPage = () => {
     const [user, setUser] = useState(undefined);
@@ -14,18 +14,13 @@ const UserPage = () => {
         api.users.getById(userId).then((data) => setUser(data));
     }, []);
 
-    // const history = useHistory();
-
-    // const handleSave = () => {
-    //     history.replace(`/users/${userId}/edit`);
-    // };
-
     if (user) {
         return (
             <div className="container">
                 <div className="row gutters-sm">
                     <div className="col-md-4 mb-3">
                         <UserCard user={user}/>
+                        <QualitiesCard data={user.qualities}/>
                     </div>
                     <div className="col-md-8">Comments</div>
                 </div>
@@ -34,11 +29,7 @@ const UserPage = () => {
         );
     } else {
         return <Loader/>;
-    };
-};
-
-UserPage.propTypes = {
-    id: PropTypes.string
+    }
 };
 
 export default UserPage;
