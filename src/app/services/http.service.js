65fa75085d4fa3db9f478a807556076f438926d6
Gregdabro <gregmonak@gmail.com>
2022-10-11 15:36:56 +0200

    6. Refresh tokens

--- src/app/services/http.service.js
+++ src/app/services/http.service.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import configFile from "../config.json";
 import { transformData } from "../utils/transformData";
+import { httpAuth } from "../hooks/useAuth";
+import localStorageService from "./localStorage.service";
 
 const http = axios.create({
     baseURL: configFile.apiEndpoint
@@ -12,6 +14,20 @@ http.interceptors.request.use(
         if (configFile.isFireBase) {
             const containSlash = /\/$/gi.test(config.url);
             config.url = (containSlash ? config.url.slice(0, -1) : config.url) + ".json";
+            const expiresDate = localStorageService.getTokenExpiresDate();
+            const refreshToken = localStorageService.getRefreshToken();
+            if (refreshToken && expiresDate < Date.now()) {
+                const { data } = await httpAuth.post("token", {
+                    grant_type: "refresh_token",
+                    refresh_token: refreshToken
+                });
+                localStorageService.setTokens({
+                    refreshToken: data.refresh_token,
+                    idToken: data.id_token,
+                    expiresIn: data.expires_id,
+                    localId: data.user_id
+                });
+            }
         }
         return config;
     }, function(error) {
